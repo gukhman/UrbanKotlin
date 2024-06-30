a8eeb3df3e513797fe816a1a3b8b70b02d41ac15
guf4k <gukhman@gmail.com>
2024-06-30 18:19:29 +0200

    getSet 30.06

--- src/Encapsulation/Card.kt
+++ src/Encapsulation/Card.kt
@@ -1,4 +1,10 @@
 class Card {
-    val number = "1234567812345678"
+    var number = "0"
+        get() {
+            print("Для получения номера карты введите пароль: ")
+            val input = readlnOrNull()
+            return if (input == pass) field
+            else "***. Пароль введен неправильно."
+        }
     private val pass = "secret"
 }
